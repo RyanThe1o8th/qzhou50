31eee5a810b8c2c595ad0c49184ab33440337f8f
Ryan Zhou <qzhou50@stuy.edu>
2024-09-13 11:01:19 -0400

    Krewes assignment

---
+++ 04_rando-dict/krewes.py
@@ -0,0 +1,11 @@
+'''
+Qianjun(Ryan) Zhou
+Boa's
+SoftDev
+K<nn> -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
+2024-09-13
+.3
+'''
+import random
+krewes = {0:"Ryan", 1:"Linda", 2:"Michelle"}
+print (random.choice(krewes))
