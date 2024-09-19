ad4b88ca41a1858ab95be5d704a6ebc7a88c09d5
Ryan Zhou <qzhou50@stuy.edu>
2024-09-19 00:13:07 -0400

    Compelted the 05 classwork

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -6,7 +6,40 @@ SoftDev
 2024-09-17
 .5
 '''
+import random
 
-f = open("krewes.txt", "r")
-for i in f:
-    print (i)
+file = open("krewes.txt", "r")
+krewes = {}
+
+data=file.read()
+
+data = data.replace("@", "$")
+outputdata = ""
+for i in range(len(data) -1):
+    if not(i != 0 and data[i] == "$" and data[i - 1] == "$"):
+        outputdata = outputdata + data[i]
+
+#print (outputdata)
+
+data = outputdata
+table = data.split("$")
+#print(table)
+keyVal = 0
+
+
+#brute force to remove index 0
+i = 0
+while i<(len(table)-1):
+    if(table[i].isnumeric()):
+        keyVal = int(table[i])
+        if (not(keyVal in krewes)):
+            krewes[keyVal] = []
+    else:
+        krewes[keyVal].append("name: " + table[i] + "\nduckie: " + table[i+1])
+        i+=1
+    i+=1
+
+a = (random.choice(list(krewes)))
+print("period: " + str(a))
+b = (random.choice(list(krewes[a])))
+print(b)
