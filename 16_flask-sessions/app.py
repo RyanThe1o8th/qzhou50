aa55d1afe5b80f307756d721b064ad41b1626b27
Ryan Zhou <qzhou50@stuy.edu>
2024-10-11 10:58:40 -0400

    finished the classwork and got cookies to work

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -9,21 +9,34 @@ time spent:
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+from flask import redirect
+from flask import session
 from flask import make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def disp_loginpage():
-    return render_template('login.html')
+    saved_name = request.cookies.get("saved_name")
+    return render_template('login.html', saved_name = saved_name)
 
 @app.route("/auth", methods = ['POST'])
 def authenticate():
-    username = request.form.get('name')
-    output = "Hello dear " + username
-    resp = make_response(output)
-    resp.set_cookie('username', username)
-    return render_template('response.html', name = request.cookies.get('username'))
+    name = request.form['name']
+    #response = (render_template("response.html", name = name))
+    response = make_response(f'hai, {name}!')
+    #response = "hi"
+    response.set_cookie("saved_name", name)
+    return response
+
+@app.route("/deleted_cookies")
+def delete_cookie():
+    response = (render_template('logout.html'))
+    #name = request.cookies.get("saved_name")
+    #response = "bye"
+    response = make_response(f'bai, {request.cookies.get("saved_name")}!')
+    response.set_cookie("saved_name", "", expires = 0)
+    return response
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
