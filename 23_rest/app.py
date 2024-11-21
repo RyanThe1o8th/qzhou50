bdbe042f4fda8b45b1b8fbb451f7e8c9879991ee
Ryan Zhou <qzhou50@stuy.edu>
2024-11-20 23:48:00 -0500

    finished all the work and everything works fine. Adding a image to a flask was fun it was very different to be working with json instead of string. Found json to be similar to that of disctionaries.

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -1,9 +1,21 @@
 import urllib.request
+import json
+from flask import Flask
+from flask import render_template
 
-data = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=V6SkvXcMUWBqpvgxFTxdhFLMp4Fp35zjX5j4Rgds")
-#print(data.geturl())
-#print(data.info())
-#print(data.read())
-data2 = data.read()
-data2 = data2.split("\n")
-print(data2)
+app = Flask(__name__)
+app.secret_key = 'haha'
+
+@app.route('/')
+def index():
+	data = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=V6SkvXcMUWBqpvgxFTxdhFLMp4Fp35zjX5j4Rgds")
+	#print(data)
+	jsonData = json.loads(data.read().decode())
+	photoData = jsonData["url"]
+	return render_template('main.html', photoLink = photoData, textInput = jsonData["explanation"])
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
+
